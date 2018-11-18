06b59133cf158f86758f7f2d92582d26032c1f7f
KamiKaze <kamilk1512@gmail.com>
2018-11-18 20:03:38 +0100

    preparing for update

--- server/init/init.js
+++
@@ -1,9 +0,0 @@
-const config = require("./config");
-const db = require("./db");
-const setting = require("./setting");
-
-module.exports = async function(){
-	config();
-	await db();
-	await setting();
-}
