227da502b35d4f49cb50d8eae3b1dc49cfa6e175
Kamil Krzysztof Kulach <kamil.kulach@winfakt.com>
2019-04-08 08:09:55 +0200

    increased body-parser limit

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,8 +12,9 @@ async function main(){
 	
 	const app = express();
 	app.use(cors());
-	app.use(bodyParser.json());
+	app.use(bodyParser.json({limit:"50mb", type:"application/json"}));
 	app.use(bodyParser.urlencoded({extended:true}));
+	app.use(bodyParser());
 
 	const mainRouter = require("./routes/main").mainRouter;
 	app.use(mainRouter);
