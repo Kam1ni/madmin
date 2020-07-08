69c35253e63b8bdb931cbe9df375923f2248c9d2
Kamil Krzysztof Kulach <kamilk1512@gmail.com>
2020-07-08 17:06:59 +0200

    Wrote some documentation for scripts

--- client/src/components/scripts/Editor.vue
+++ client/src/components/scripts/Editor.vue
@@ -6,7 +6,8 @@
 					<v-flex xs12 md6>
 						<v-text-field v-model="script.name" label="Name" :rules="nameRules"></v-text-field>
 					</v-flex>
-				
+					<v-spacer/>
+					<v-btn icon @click="showInfo = true"><v-icon color="accent">info</v-icon></v-btn>
 				</v-layout>
 
 				<v-layout row>
@@ -35,15 +36,23 @@
 						<v-select :items="minutes" label="Minut" v-model="script.minut"/>
 					</v-flex>
 				</v-layout>
+
+				<v-layout row>
+					<v-spacer/>
+				</v-layout>
 			</v-card-text>
-			async function(madmin, require, ...args){
+
 			<codemirror v-model="script.code" class="editor"></codemirror>
-			}
+
 			<v-card-actions>
 				<v-spacer/>
 				<v-btn depressed color="accent" @click="cancel">Cancel</v-btn>
 				<v-btn depressed color="primary" @click="save">Save</v-btn>
 			</v-card-actions>
+
+			<v-dialog v-model="showInfo" max-width="900">
+				<app-editor-info @close="showInfo = false"/>
+			</v-dialog>
 		</v-form>
 	</v-card>
 </template>
@@ -55,6 +64,7 @@ import { isNullOrUndefined } from 'util';
 import { stringHasWhiteSpace } from '@/functions/string';
 import { scriptService } from '../../services/script-service';
 import { AppConfig } from '../../conifg';
+import AppEditorInfo from "./EditorInfo.vue";
 
 interface ISelectItem {
 	text:string;
@@ -70,7 +80,8 @@ export default Vue.extend({
 				(v:string) => !stringHasWhiteSpace(v) || "Path may not have spaces",
 				(v:string) => !scriptService.scriptNameInUse(this.script.name, this.script.id) || "Path must be unique"
 			],
-			usesMinutesInterval: AppConfig.scriptsRunAtMinutIntervals
+			usesMinutesInterval: AppConfig.scriptsRunAtMinutIntervals,
+			showInfo: false
 		}
 	},
 	props:{
@@ -145,8 +156,10 @@ export default Vue.extend({
 			}
 			return arr;
 		}
-
 	},
+	components:{
+		AppEditorInfo
+	}
 })
 </script>
 
---
+++ client/src/components/scripts/EditorInfo.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-card>
+		<v-card-title class="headline">Writing scripts</v-card-title>
+		<v-card-text>
+			Madmin scripts are not much more then JavaScript code saved as a string.
+			The code is converted before execution into a JavaScript AsyncFunction. 
+			This means you can use await inside the script.
+			On execution the next three arguments are passed into the script.
+			<ul>
+				<li>madmin: a static object where you can store volatile data in or make queries (More info comming soon)</li>
+				<li>require: the standard Node.js require function</li>
+				<li>args: a list of string args that you can send before execution</li>
+			</ul>
+		</v-card-text>
+		<v-card-actions>
+			<v-spacer/>
+			<v-btn flat color="accent" @click="close">Close</v-btn>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+	methods:{
+		close(){
+			this.$emit("close")
+		}
+	}
+})
+</script>
